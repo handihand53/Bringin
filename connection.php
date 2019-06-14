aceeb4255406fc00f08d10db204f5167b3e42350
Handi Hermawan <handihand53@gmail.com>
2019-06-14 22:47:25 +0700

    add connection file

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+    $username="root";
+    $password="";
+    $server="localhost"
+    $database="bringinDb";
+
+    $con = mysqli_connect($server, $username, $password, $database) or die("Connection error");
+?>
